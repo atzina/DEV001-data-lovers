c9e0f8e01d6c1c841fc389dc17eb2c0d280b7ddd
Addidaan <adriana48_rs@hotmail.com>
2022-10-28 20:35:45 -0500

    main data y spec con funcion sort

--- src/data.js
+++ src/data.js
@@ -1,10 +1,35 @@
-// estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';
+//import datacopy from './data/ghibli/ghibli2.js';
+//console.log(data.films);
 
-//export const example = () => {
-//return 'example';
-//};
+//const pelis = datacopy.films;
+
+export const order = (movies) => {
+    return movies.sort((a, b) => {
+
+        if (a.title < b.title) {
+            return -1;
+        } else if (a.title > b.title) {
+            return 1;
+
+        }
+    })
+
+};
+//console.log(order(pelis));
 
 //export const anotherExample = () => {
 //return 'OMG';
 //};
+
+
+
+// const muvis = data.films
+
+// export const filteredMuvis = muvis.filter(function (muvis) {
+//     return muvis.director == "Hayao Miyazaki";
+    
+// });
+// //console.log(filteredMuvis);
+
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,31 @@
-import { example, anotherExample } from '../src/data.js';
+import { order} from '../src/data.js';
+//import { films } from './data/ghibli/ghibli.js';
+//import datacopy from './data/ghibli/ghibli2.js'
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+
+
+describe('quiero testear sort', () => {
+  it('que order sea funcion', () => {
+    expect(typeof order).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('tiene que ordenar a-z`', () => {
+    const nombrePruebas = [
+      {title: "Whisper of the Heart" }, {title: "Pom Poko" }, {title: "Castle in the Sky" }, {title: "My Neighbor Totoro" }
+    ]
+    const resultado = [{ title: "Castle in the Sky" }, { title: "My Neighbor Totoro" }, { title: "Pom Poko" }, { title: "Whisper of the Heart" }]
+    expect(order(nombrePruebas)).toEqual(resultado);
   });
 });
+
+
+// describe('quiero testear filter', () => {
+//   it('que sea funcion', () => {
+//     expect(typeof filteredMovies).toEqual('function');
+//   });
+
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toEqual('OMG');
+//   });
+// });
